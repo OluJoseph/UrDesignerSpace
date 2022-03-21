16397aa8580880917e06b075cd31dc1f4668e0be
unknown <oluwatobiloba8624@gmail.com>
2022-03-21 08:00:44 +0100

    reverting

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@
 
 const Card = (props) => {
     return (
-        <a href={props.url} target="_blank">
+        <a href={props.url}>
             <div style={{backgroundImage: `url(${props.image})`}} className="card grow">
                 <div href={props.url}>
                     <h6>{props.title}</h6>
