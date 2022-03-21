ee6d620e22f1927dd3dfab5570e002473778a3a5
unknown <oluwatobiloba8624@gmail.com>
2022-03-21 10:41:23 +0100

    sigh

--- Cards.js
+++ Cards.js
@@ -7,7 +7,7 @@ var Cards = function Cards(_ref) {
         "div",
         { className: "cards container-fluid" },
         cardsToDisplay.map(function (card) {
-            return React.createElement(Card, { key: card.id, category: card.category, image: card.img, title: card.title, tools: card.tools, myUrl: card.url });
+            return React.createElement(Card, { key: card.id, category: card.category, image: card.img, title: card.title, tools: card.tools, url: card.url });
         })
     );
 };
